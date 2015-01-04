4de0a88fc608cf240069127527b4ce535c38510a
kei-s <kei.shiratsuchi@gmail.com>
2015-01-04 14:49:46 +0900

    Refactoring spec

--- spec/equal/cards_spec.rb
+++ spec/equal/cards_spec.rb
@@ -5,24 +5,22 @@ describe Equal::Cards do
 
   describe 'new' do
     subject { equal }
+    let(:c) {|example| example.example_group.description }
     context '0123456789+-*/' do
-      let(:c) { '0123456789+-*/' }
       it { expect{ subject }.not_to raise_error }
     end
     context '?' do
-      let(:c) { '?' }
       it { expect{ subject }.to raise_error(ArgumentError) }
     end
   end
 
   describe '#answers' do
     subject { equal.answers.to_a }
+    let(:c) {|example| example.example_group.description }
     context '11' do
-      let(:c) { '11' }
       it { expect(subject).to eq [] }
     end
     context '111' do
-      let(:c) { '111' }
       it { expect(subject).to eq ['1=1'] }
     end
   end
--- spec/equal/solver_spec.rb
+++ spec/equal/solver_spec.rb
@@ -2,16 +2,21 @@ require 'spec_helper'
 
 describe Equal::Solver do
   let(:solver) { Equal::Solver.new(p) }
+  let(:p) {|example| example.example_group.description.split('') }
 
   describe '#solve' do
     subject { solver.solve }
     context '11' do
-      let(:p) { '11'.split('') }
       it { expect(subject).to eq [] }
     end
     context '111' do
-      let(:p) { '111'.split('') }
       it { expect(subject).to eq ['1','=','1'] }
+    end
+    context '112' do
+      it { expect(subject).to eq [] }
+    end
+    context '1+11' do
+      it { expect(subject).to eq [] }
     end
   end
 end
